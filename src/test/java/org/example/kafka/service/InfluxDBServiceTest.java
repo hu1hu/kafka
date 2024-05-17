953a27cc5e1461c1294417b43e32aba7d3d0c836
hu1hu <3261801992@qq.com>
2024-05-17 17:03:23 +0800

    delete test

--- src/test/java/org/example/kafka/service/InfluxDBServiceTest.java
+++ src/test/java/org/example/kafka/service/InfluxDBServiceTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public class InfluxDBServiceTest {
-    @Autowired
-    private InfluxDBService influxDBService;
-
-    @Test
-    void writeData() {
-        //数据格式 <measurement>[,<tag-key>=<tag-value>...] <field-key>=<field-value>[,<field2-key>=<field2-value>...] [unix-nano-timestamp]
-        influxDBService.writeDataByString("mem,host=host1 used_percent=25");
-    }
-}
+//@SpringBootTest
+//public class InfluxDBServiceTest {
+//    @Autowired
+//    private InfluxDBService influxDBService;
+//
+//    @Test
+//    void writeData() {
+//        //数据格式 <measurement>[,<tag-key>=<tag-value>...] <field-key>=<field-value>[,<field2-key>=<field2-value>...] [unix-nano-timestamp]
+//        influxDBService.writeDataByString("mem,host=host1 used_percent=25");
+//    }
+//}
